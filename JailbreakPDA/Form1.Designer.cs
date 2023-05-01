f8a6619a3107dd6e92ec3e184ca2805d140004c2
e1z0 <e1z0@vintage2000.org>
2023-05-01 17:10:10 +0300

    public

---
+++ JailbreakPDA/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace JailbreakPDA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Jailbreak!";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 20);
+            this.label1.Text = "Ready";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(3, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 20);
+            this.label2.Text = "Current val:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 294);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Jailbreak PDA";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ JailbreakPDA/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+/*
+ * Just a simple tool that replaces the default launch application on WinCE embedded devices with windows explorer 
+ * If no explorer was found on the system then throws error before procedding, also includes some winapi function to flush registry entries to nand for sure
+ * (c) e1z0 2023
+ * Possible shells:
+ * shell32.exe
+ * explorer.exe
+ * fexplore.exe
+ * TODO
+ * add checking if the device are already jailbroken
+ */
+
+namespace JailbreakPDA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        const string root = "HKEY_LOCAL_MACHINE";
+        const string regpath = @"\init";
+        const string key = "Launch50";
+
+        [Flags]
+        public enum ExitFlags
+        {
+            Reboot = 0x02,
+            PowerOff = 0x08
+        }
+
+        // flush registry to nand
+        [System.Runtime.InteropServices.DllImport("coredll.dll")]
+        public static extern int RegFlushKey(IntPtr hKey);
+        // reboot option
+        [System.Runtime.InteropServices.DllImport("coredll.dll")]
+        public static extern Int32 SetSystemPowerState(Char[] psState, Int32 StateFlags, Int32 Options);
+        // enable taskbar functions
+        [System.Runtime.InteropServices.DllImport("coredll.dll")]
+        public static extern IntPtr FindWindow(string className, string WindowName);
+        [System.Runtime.InteropServices.DllImport("coredll.dll")]
+        public static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+        [System.Runtime.InteropServices.DllImport("coredll.dll")]
+        public static extern bool EnableWindow(IntPtr hwnd, bool bEnable);
+
+
+
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var val = "fexplore.exe";
+            var path = "\\windows\\";
+            var found = false;
+
+            label1.Text = "Changing configuration";
+            
+            if (File.Exists(path+val)) {
+                found = true;
+            }
+
+            val = "explorer.exe";
+            if (File.Exists(path+val))
+            {
+                found = true;
+            }
+            
+
+            if (!found)
+            {
+                label1.Text = "Jailbreak was unsuccessul";
+                MessageBox.Show("Explorer or compatible shell was not found on the windows directory!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button1);
+                return;
+            }
+
+
+            // change registry key
+            Registry.SetValue(root+"\\"+regpath,key,val,RegistryValueKind.String);
+
+            // flush to nand
+            const UInt32 HKEY_LOCAL_MACHINE = 0x80000002;
+            IntPtr hkeyLocalMachine = new IntPtr(unchecked((int)HKEY_LOCAL_MACHINE));
+            RegFlushKey(hkeyLocalMachine);
+
+            // enable taskbar
+            IntPtr hwnd = FindWindow("HHTaskBar", null);
+            EnableWindow(hwnd, true);
+            ShowWindow(hwnd, 1);
+
+            // enable command prompt
+            var rkey = Registry.LocalMachine.OpenSubKey("Drivers\\Console",true);
+            rkey.SetValue("OutputTo",0);
+
+            label1.Text = "Great Success!";
+            MessageBox.Show("Done","Success",MessageBoxButtons.OK,MessageBoxIcon.Asterisk,MessageBoxDefaultButton.Button1);
+            // reboot
+            //const int POWER_STATE_RESET = 0x800000;
+            //SetSystemPowerState(null, POWER_STATE_RESET, 0);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var currentval = Registry.GetValue(root + "\\" + regpath, key, null) as string;
+            label2.Text = "Current shell: "+currentval;
+        }
+    }
+}
